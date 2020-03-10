202878becb3e0fbc757071b757769be0349e9ce6
Chris B <ceball@users.sf.net>
2020-03-10 16:53:09 +0000

    try fewer deps for gh

--- setup.py
+++ setup.py
@@ -14,48 +14,6 @@ install_requires = ['param>=1.8.0,<2.0', 'numpy>=1.0', 'pyviz_comms>=0.7.2', 'pa
 
 extras_require = {}
 
-# Notebook dependencies
-extras_require['notebook'] = ['ipython>=5.4.0', 'notebook']
-
-# IPython Notebook + pandas + matplotlib + bokeh
-extras_require['recommended'] = extras_require['notebook'] + [
-    'pandas', 'matplotlib>=2.2', 'bokeh>=1.1.0,<2.0.0']
-
-# Requirements to run all examples
-extras_require['examples'] = extras_require['recommended'] + [
-    'networkx', 'pillow', 'xarray>=0.10.4', 'plotly>=4.0',
-    'datashader', 'selenium', 'phantomjs', 'ffmpeg', 'streamz>=0.5.0',
-    'cftime', 'netcdf4', 'bzip2', 'dask', 'scipy']
-
-if sys.version_info.major > 2:
-    extras_require['examples'].append('spatialpandas')
-
-# Extra third-party libraries
-extras_require['extras'] = extras_require['examples']+[
-    'cyordereddict', 'pscript==0.7.1']
-
-# Test requirements
-extras_require['tests'] = [
-    'nose', 'mock', 'flake8==3.6.0', 'coveralls', 'path.py',
-    'matplotlib>=2.2,<3.1', 'nbsmoke >=0.2.0', 'pytest-cov ==2.5.1']
-
-extras_require['unit_tests'] = extras_require['examples']+extras_require['tests']
-
-extras_require['basic_tests'] = extras_require['tests']+[
-    'matplotlib>=2.1', 'bokeh>=1.1.0', 'pandas']+extras_require['notebook']
-
-extras_require['nbtests'] = extras_require['recommended'] + [
-    'nose', 'awscli', 'deepdiff', 'nbconvert==5.3.1', 'jsonschema==2.6.0',
-    'cyordereddict', 'ipython==5.4.1']
-
-extras_require['doc'] = extras_require['examples'] + [
-    'nbsite>0.5.2', 'sphinx', 'sphinx_holoviz_theme', 'mpl_sample_data', 'awscli', 'pscript']
-
-extras_require['build'] = ['param >=1.7.0', 'setuptools >=30.3.0', 'pyct >=0.4.4']
-
-# Everything including cyordereddict (optimization) and nosetests
-extras_require['all'] = list(set(extras_require['unit_tests']) | set(extras_require['nbtests']))
-
 
 def get_setup_version(reponame):
     """
